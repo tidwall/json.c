17843e80ad7b63c600653366369e2f8ec22e03e4
tidwall <joshbaker77@gmail.com>
2023-08-03 14:53:17 -0700

    Update ludo macros

--- json.c
+++ json.c
@@ -99,16 +99,15 @@ static inline int64_t vesc(const uint8_t *json, int64_t jlen, int64_t i) {
 #define ludo4(i,f) ludo2(i,f); ludo2(i,f);
 #define ludo8(i,f) ludo4(i,f); ludo4(i,f);
 #define ludo16(i,f) ludo8(i,f); ludo8(i,f);
+#define ludo32(i,f) ludo16(i,f); ludo16(i,f);
+#define ludo64(i,f) ludo32(i,f); ludo32(i,f);
 #define for1(i,n,f) while(i+1<=(n)) { ludo1(i,f); }
 #define for2(i,n,f) while(i+2<=(n)) { ludo2(i,f); } for1(i,n,f);
 #define for4(i,n,f) while(i+4<=(n)) { ludo4(i,f); } for1(i,n,f);
 #define for8(i,n,f) while(i+8<=(n)) { ludo8(i,f); } for1(i,n,f);
 #define for16(i,n,f) while(i+16<=(n)) { ludo16(i,f); } for1(i,n,f);
-#define for1c(i,n,c,f) while((c)&&i+1<=(n)) { ludo1(i,f); }
-#define for2c(i,n,c,f) while((c)&&i+2<=(n)) { ludo2(i,f); } for1c(i,n,c,f);
-#define for4c(i,n,c,f) while((c)&&i+4<=(n)) { ludo4(i,f); } for1c(i,n,c,f);
-#define for8c(i,n,c,f) while((c)&&i+8<=(n)) { ludo8(i,f); } for1c(i,n,c,f);
-#define for16c(i,n,c,f) while((c)&&i+16<=(n)) { ludo16(i,f); } for1c(i,n,c,f);
+#define for32(i,n,f) while(i+32<=(n)) { ludo32(i,f); } for1(i,n,f);
+#define for64(i,n,f) while(i+64<=(n)) { ludo64(i,f); } for1(i,n,f);
 #endif
 
 static const uint8_t strtoksu[256] = {
